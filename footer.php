0fe13cc5390a4d032b6a24c36ca1e0a30b98dc64
Kalyanram P <kalyanram.p.12@gmail.com>
2024-10-12 22:38:36 +0530

    footer added

---
+++ footer.php
@@ -0,0 +1,17 @@
+<style>
+    .foot{
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        height: 3vh;
+        width:100%;
+        color:#042A38;
+        background-color:#fff;
+        font-weight:bolder;
+        margin:0;
+        margin-top: 1vh;
+    }
+</style>
+<div class="foot">
+    <center>&copy; 2024 kram-12, Inc. All rights reserved.</center>
+</div>
